66e9a6a043ea4d4c1f5a88e9a2dcfae7e1d342d1
Maciek Kaczmarczyk <maciejkaczmarczyk95@gmail.com>
2018-08-28 20:08:17 +0200

    input refactor

--- packages/app/src/components/ui/Input/Input.tsx
+++ packages/app/src/components/ui/Input/Input.tsx
@@ -1,32 +1,64 @@
 import { Form, Input as AInput } from 'antd';
+import _isFunction from 'lodash/isFunction';
 import * as React from 'react';
 import noop from '../../../utils/noop';
 
-const Input: React.SFC<IPropTypes> = ({
-  onChange,
-  onBlur,
-  name,
-  value,
-  errorText,
-  showError,
-  placeholder,
-}) => (
-  <Form.Item
-    validateStatus={showError ? 'error' : undefined}
-    help={errorText && showError ? errorText : undefined}
-  >
-    <AInput
-      name={name}
-      onChange={onChange}
-      onBlur={onBlur}
-      value={value}
-      placeholder={placeholder}
-    />
-  </Form.Item>
-)
+class Input extends React.PureComponent<IPropTypes> {
+  private help?: string
+  private showError?: 'error' | undefined
+
+  static defaultProps = {
+    errorText: '',
+    name: '',
+    onBlur: noop,
+    onChange: noop,
+    placeholder: '',
+    showError: false,
+    value: '',
+  };
+
+  constructor(props: IPropTypes) {
+    super(props);
+    
+    if (props.errorText && props.showError) {
+      this.help = props.errorText;
+    }
+
+    if (this.props.showError) {
+      this.showError = 'error';
+    } else {
+      this.showError = undefined;
+    }
+  }
+
+  onInputChange = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    
+    if (this.props.onChange && _isFunction(this.props.onChange)) {
+      this.props.onChange(target.value);
+    }
+  }
+
+  render() {
+    return (
+      <Form.Item
+        validateStatus={this.showError}
+        help={this.help}
+      >
+        <AInput
+          name={this.props.name}
+          onChange={this.onInputChange}
+          onBlur={this.props.onBlur}
+          value={this.props.value}
+          placeholder={this.props.placeholder}
+        />
+      </Form.Item>
+    );
+  }
+};
 
 interface IPropTypes {
-  onChange?: (e: React.SyntheticEvent) => void,
+  onChange?: (arg1: string) => void,
   name?: string,
   onBlur?: () => void,
   value?: string,
@@ -34,15 +66,5 @@ interface IPropTypes {
   showError?: boolean,
   placeholder?: string,
 };
-
-Input.defaultProps = {
-  errorText: '',
-  name: '',
-  onBlur: noop,
-  onChange: noop,
-  placeholder: '',
-  showError: false,
-  value: '',
-}
 
 export default Input;
